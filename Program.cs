c070f083543195d862e3cd40fbdffa19c9fc1f32
Niteshvarmaa <niteshvarma126@gmail.com>
2024-02-16 10:43:21 +0530

    Second Commit

--- Program.cs
+++ Program.cs
@@ -3,5 +3,7 @@ using CalcExample;
 Calc calc = new Calc();
 //public delegate void CalcDel(double num1, double num2);
 //CalcDel del = new CalcDel(calc.Add);
-Console.WriteLine("Inocation");
+Console.WriteLine("Add Call");
 calc.Add(12, 45);
+Console.WriteLine("Multiply Call");
+calc.Multi(10.5, 12.45);
