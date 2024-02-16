4d2c63bec05e6c98982f25cbc8d2fe3f842adb8a
Niteshvarmaa <niteshvarma126@gmail.com>
2024-02-16 10:11:52 +0530

    First Commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CalcExample;
+Calc calc = new Calc();
+//public delegate void CalcDel(double num1, double num2);
+//CalcDel del = new CalcDel(calc.Add);
+Console.WriteLine("Inocation");
+calc.Add(12, 45);
