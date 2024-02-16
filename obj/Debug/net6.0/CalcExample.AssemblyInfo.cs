78c82055fd7d48a72daef4c7fa360705cb4a318e
Niteshvarmaa <niteshvarma126@gmail.com>
2024-02-16 11:04:20 +0530

    Third Commit

--- obj/Debug/net6.0/CalcExample.AssemblyInfo.cs
+++ obj/Debug/net6.0/CalcExample.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalcExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d2c63bec05e6c98982f25cbc8d2fe3f842adb8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c070f083543195d862e3cd40fbdffa19c9fc1f32")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalcExample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalcExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
